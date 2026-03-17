9e9c70d338096eb719b9f4184f360a1f0ee181e0
nfarukzengin <nfarukzengin@gmail.com>
2026-03-17 09:21:10 +0300

    Create constants.py

---
+++ brands/fresh_scarfs/constants.py
@@ -0,0 +1,7 @@
+# Fresh Scarfs Dosya ID'leri
+SHEET_IDS = {
+    "Google ADS": "1V1_YvNbW3W2qOlk1SnLaPOgSUMO_tuShCXtX81yPEgA",
+    "meta": "META_REKLAM_DOSYA_ID",
+    "trendyol": "TRENDYOL_DOSYA_ID",
+    "stok": "STOK_TAKIP_DOSYA_ID"
+}
