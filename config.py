8f1d3bc3576caed756a4c800d3d28320a06573fb
nfarukzengin <nfarukzengin@gmail.com>
2026-03-17 09:14:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-
+# Sabit API Bilgileri
+API_URL = " https://script.google.com/macros/s/AKfycbwPSkft-3lxwcq85HC805Uin0yI-i-d1hcmmGEHCpbfdhiNzgxQ8ZSDS7bsklO3E7qD/exec"
+TOKEN = "manuka-fresh-reports"
